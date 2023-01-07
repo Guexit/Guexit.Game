8a7fc144c7ad50633a5655727b018938383abcd4
Pablo Company <p.c.ramirez5150@gmail.com>
2023-01-07 23:20:28 +0100

    feat: Subscribes to message from Identity Provider by adding messages package

---
+++ src/TryGuessIt.Game.WebApi/UserCreatedConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using TryGuessIt.IdentityProvider.Messages;
+
+namespace TryGuessIt.Game.WebApi;
+
+public class UserCreatedConsumer : IConsumer<UserCreated>
+{
+    private readonly ILogger<UserCreatedConsumer> _logger;
+
+    public UserCreatedConsumer(ILogger<UserCreatedConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<UserCreated> context)
+    {
+        _logger.LogInformation("Handling UserCreatedIntegrationEvent. Id: '{userId}'", context.Message.Id);
+        return Task.CompletedTask;
+    }
+}
