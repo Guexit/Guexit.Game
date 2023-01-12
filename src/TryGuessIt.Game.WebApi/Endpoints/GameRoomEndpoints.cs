98680b48d1c3d551a880a9f1c058d8a3f179df1f
Pablo Company <p.c.ramirez5150@gmail.com>
2023-01-12 21:08:26 +0100

    feat: Adds endpoints definition

---
+++ src/TryGuessIt.Game.WebApi/Endpoints/GameRoomEndpoints.cs
@@ -0,0 +1,21 @@
+﻿namespace TryGuessIt.Game.WebApi.Endpoints;
+
+public static class GameRoomEndpoints
+{
+    public static void MapGameRoomEndpoints(this IEndpointRouteBuilder app)
+    {
+        // TODO add versioning
+        app.MapPost("v1/game-rooms", CreateGameRoom);
+        app.MapPost("v1/game-rooms/{gameRoomId}/join", JoinGameRoom);
+    }
+
+    private static IResult CreateGameRoom()
+    {
+        return Results.BadRequest();
+    }
+
+    private static IResult JoinGameRoom(string gameRoomId)
+    {
+        return Results.BadRequest();
+    }
+}
