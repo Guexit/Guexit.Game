c83b46734235d4c28b047c2f87227ba3bc42281e
Pablo Company <p.c.ramirez5150@gmail.com>
2023-02-23 19:20:41 +0100

    Fix docker compose

--- src/Guexit.Game.Application/Services/PlayerManagementService.cs
+++ src/Guexit.Game.Application/Services/PlayerManagementService.cs
@@ -18,7 +18,8 @@ public sealed class PlayerManagementService : IPlayerManagementService
 
     public async Task CreatePlayer(PlayerId playerId, string username, CancellationToken cancellationToken = default)
     {
-        if (await _playerRepository.GetBy(playerId, cancellationToken) is not null)
+        var existingPlayer = await _playerRepository.GetBy(playerId, cancellationToken);
+        if (existingPlayer is not null)
             return;
 
         await _playerRepository.Add(new Player(playerId, username), cancellationToken);
