f5ec9b70013a6515c2134256785fa5d8dffeb377
Pablo Company <p.c.ramirez5150@gmail.com>
2024-01-31 15:18:38 +0100

    fix: Uses custom cloud role name to segment telemetry by service #78

---
+++ src/Guexit.Game.WebApi/DependencyInjection/ApplicationInsightsInstaller.cs
@@ -0,0 +1,19 @@
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Guexit.Game.WebApi.DependencyInjection;
+
+public static class ApplicationInsightsInstaller
+{
+    public static IServiceCollection AddAppInsights(this IServiceCollection services)
+    {
+        services.AddApplicationInsightsTelemetry();
+        services.AddSingleton<ITelemetryInitializer, GuexitFrontendTelemetryInitializer>();
+        return services;
+    }
+    
+    public sealed class GuexitFrontendTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry) => telemetry.Context.Cloud.RoleName = "Frontend";
+    }
+}
