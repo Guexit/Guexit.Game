ff1cc5a9840d2d7826f4365f9a9e531dd524fe1e
Pablo Company <p.c.ramirez5150@gmail.com>
2024-05-29 13:21:58 +0200

    feat: Publishes events to message broker #139

---
+++ src/Guexit.Game.Producers/CardReRollReservedProducer.cs
@@ -0,0 +1,22 @@
+﻿using Guexit.Game.Domain;
+using Guexit.Game.Domain.Model.GameRoomAggregate.Events;
+using Guexit.Game.Messages;
+using MassTransit;
+
+namespace Guexit.Game.Producers;
+
+public sealed class CardReRollReservedProducer : IDomainEventHandler<CardReRollReserved>
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public CardReRollReservedProducer(IPublishEndpoint publishEndpoint) => _publishEndpoint = publishEndpoint;
+
+    public async ValueTask Handle(CardReRollReserved @event, CancellationToken ct = default)
+    {
+        await _publishEndpoint.Publish(new CardReRollReservedIntegrationEvent
+        {
+            GameRoomId = @event.GameRoomId,
+            PlayerId = @event.PlayerId
+        }, ct);
+    }
+}
---
+++ tests/Guexit.Game.Producers.UnitTests/WhenHandlingCardReRollCompleted.cs
@@ -0,0 +1,38 @@
+﻿using Guexit.Game.Domain.Model.GameRoomAggregate;
+using Guexit.Game.Domain.Model.GameRoomAggregate.Events;
+using Guexit.Game.Domain.Model.PlayerAggregate;
+using Guexit.Game.Messages;
+using MassTransit;
+using NSubstitute;
+
+namespace Guexit.Game.Producers.UnitTests;
+
+public sealed class WhenHandlingCardReRollCompleted
+{
+    private readonly CardReRollCompletedProducer _eventHandler;
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public WhenHandlingCardReRollCompleted()
+    {
+        _publishEndpoint = Substitute.For<IPublishEndpoint>();
+        _eventHandler = new CardReRollCompletedProducer(_publishEndpoint);
+    }
+
+    [Fact]
+    public async Task CardReRollCompletedIntegrationEventIsPublished()
+    {
+        var gameRoomId = new GameRoomId(Guid.NewGuid());
+        var playerId = new PlayerId("player-123");
+        var oldCardId = new CardId(Guid.NewGuid());
+        var newCardId = new CardId(Guid.NewGuid());
+        var cardReRollCompleted = new CardReRollCompleted(gameRoomId, playerId, oldCardId, newCardId);
+
+        await _eventHandler.Handle(cardReRollCompleted);
+
+        await _publishEndpoint.Received(1).Publish(Arg.Is<CardReRollCompletedIntegrationEvent>(e =>
+            e.GameRoomId == gameRoomId.Value &&
+            e.PlayerId == playerId.Value &&
+            e.OldCardId == oldCardId.Value &&
+            e.NewCardId == newCardId.Value));
+    }
+}
