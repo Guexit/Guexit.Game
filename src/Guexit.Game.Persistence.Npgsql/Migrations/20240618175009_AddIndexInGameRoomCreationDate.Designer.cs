b9c33c69707b18285e710388ada92d658d46d359
Pablo Company <p.c.ramirez5150@gmail.com>
2024-06-18 19:50:38 +0200

    feat: Adds index in game room created at and orders available game rooms by creation date descendant #148

--- src/Guexit.Game.Persistence.Npgsql/Migrations/20240618175009_AddIndexInGameRoomCreationDate.Designer.cs
+++ src/Guexit.Game.Persistence.Npgsql/Migrations/20240618175009_AddIndexInGameRoomCreationDate.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Guexit.Game.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Guexit.Game.Persistence.Npgsql.Migrations
 {
     [DbContext(typeof(GameDbContext))]
-    partial class GameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240618175009_AddIndexInGameRoomCreationDate")]
+    partial class AddIndexInGameRoomCreationDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
