ccbb6a20c347bcdce9cdee2d3a6be1ecb1077d2c
Pablo Company <pablocompany@Pablos-MacBook-Pro.local>
2024-10-24 12:24:03 +0200

    poc: Spin up a PostgreSQL server with TestContainers in component tests

--- tests/Guexit.Game.Component.IntegrationTests/GameWebApplicationFactory.cs
+++ tests/Guexit.Game.Component.IntegrationTests/GameWebApplicationFactory.cs
@@ -1,4 +1,6 @@
-﻿using Guexit.Game.Component.IntegrationTests.Extensions;
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Guexit.Game.Component.IntegrationTests.Extensions;
 using Guexit.Game.Component.IntegrationTests.TestDoubles;
 using Guexit.Game.Consumers;
 using Guexit.Game.Domain;
@@ -9,14 +11,51 @@ using MassTransit.EntityFrameworkCoreIntegration;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Guexit.Game.Component.IntegrationTests;
 
-public sealed class GameWebApplicationFactory : WebApplicationFactory<Game.WebApi.IAssemblyMarker>
+public sealed class GameWebApplicationFactory : WebApplicationFactory<Game.WebApi.IAssemblyMarker>, IAsyncLifetime
 {
+    private IContainer? _postgreSqlContainer;
+    
+    public async Task InitializeAsync()
+    {
+        _postgreSqlContainer = new ContainerBuilder()
+            .WithName("guexit_game_database_test")
+            .WithImage("postgres:16.4")
+            .WithEnvironment("POSTGRES_USER", "postgres")
+            .WithEnvironment("POSTGRES_PASSWORD", "postgres")
+            .WithEnvironment("POSTGRES_DB", "guexit_game_database_test")
+            .WithPortBinding(5555, 5432)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5432))
+            .Build();
+        
+        await _postgreSqlContainer.StartAsync();
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        if (_postgreSqlContainer is not null) 
+            await _postgreSqlContainer.DisposeAsync();
+    }
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureAppConfiguration(configurationBuilder =>
+        {
+            var dbConnectionString =
+                $"User ID=postgres;Password=postgres;Host=localhost;Port={_postgreSqlContainer!.GetMappedPublicPort(5432)};Database=guexit_game_database_test;";
+            
+            var configurationOverrides = new Dictionary<string, string?>
+            {
+                { "ConnectionStrings:Guexit_Game_GameDb", dbConnectionString },
+                { "Database:MigrateOnStartup", "true" }
+            };
+            configurationBuilder.AddInMemoryCollection(configurationOverrides);
+        });
+        
         builder.ConfigureTestServices(services =>
         {
             services.AddMassTransitTestHarness();
